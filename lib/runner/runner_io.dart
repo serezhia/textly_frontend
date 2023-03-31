0d2a9a45c8767df3f62e9728ded2f03218d5313c
serezhia <dev@serezhia.ru>
2023-03-31 04:35:28 +0300

    [qf] - rename runApp func in io runner

--- lib/runner/runner_io.dart
+++ lib/runner/runner_io.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:textly/src/app.dart';
 
 /// Запуск приложения как io
-Future<void> run() async {
+Future<void> runApp() async {
   // Перехватывать ошибки флатера в релизе
   final sourceFlutterError = FlutterError.onError;
   FlutterError.onError = (details) {
